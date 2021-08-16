fb23851945aabc8eeccaddaa5ca27a24be06131c
Samyuktha Sudhakar <samyuktha.sudhakar@gmail.com>
2021-08-16 21:08:00 +0530

    refactor

--- isTriangle.js
+++ isTriangle.js
@@ -12,10 +12,10 @@ function checkIfTriangle(){
     const sumValue = calculateSumOfAngles(Number(angles[0].value),Number(angles[1].value),Number(angles[2].value));
     console.log(sumValue);
     if (sumValue===180){
-        output.innerText = "Yay, the angle values entered make a triangle 🥳";
+        output.innerText = "Yay, the angle values form a triangle 🥳";
     }
     else{
-        output.innerText = "Oops, the angles entered don't form a triangle 😓";
+        output.innerText = "Oops, the angles don't form a triangle 😓";
     }
 }
 
