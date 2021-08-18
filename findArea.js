4c69258e6a207bc0e3843c0300cb5af0e256ed8c
Samyuktha Sudhakar <samyuktha.sudhakar@gmail.com>
2021-08-18 21:28:14 +0530

    refactor : change area formula

--- findArea.js
+++ findArea.js
@@ -5,9 +5,15 @@ const calculate = document.querySelector("#find-Area");
 const result = document.querySelector("#area");
 
 function calculateArea(){
-    console.log("Huston, we have connection");
-    const area = (Number(sides[0].value) * Number(sides[1].value))/2;
-    console.log("The area of the triangle is " + area + " sq units.")
+
+    var a = Number(sides[0].value);
+    var b = Number(sides[1].value);
+    var c = Number(sides[2].value);
+    var s = (a+b+c)/2
+
+    var value = s*(s-a)*(s-b)*(s-c);
+    var area = Math.sqrt(value);
+
     result.innerText = "The area of the triangle is " + area + " sq units.";
 }
 
